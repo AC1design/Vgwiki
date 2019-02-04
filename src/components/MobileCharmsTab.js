9c1534c2ab3a963096883874f26b3b14e218f3d2
Skillz <gankstarsleague@gmail.com>
2019-02-04 09:20:31 -0500

    refactoring the mobile charms section with constants and map

--- src/components/MobileCharmsTab.js
+++ src/components/MobileCharmsTab.js
@@ -1,41 +1,24 @@
 import React from 'react'
-import Emojibox from './Emoji';
+import Emojibox from './Emoji'
+import { charms } from '../constants/charms'
 
 const Charms = props => (
-    <div>
-            <div className="Selectan">
-                <h1>Emoji Charms</h1>
-            </div>
-            <div className='EmojiboxGroupScroll'>
-        <div className='EmojiboxGroup'>
-
-            <Emojibox
-                itemimg={require('./../images/Emoji/CallMe.png')}
-                itemname="Call Me"
-            />
-            <Emojibox
-                itemimg={require('./../images/Emoji/Heart_Broken.png')}
-                itemname="Broken Heart"
-            />
-            <Emojibox
-                itemimg={require('./../images/Emoji/Heart.png')}
-                itemname="Heart"
-            />
-            <Emojibox
-                itemimg={require('./../images/Emoji/Candy_Shop.png')}
-                itemname="Kiss"
-            />
-            <Emojibox
-                itemimg={require('./../images/Emoji/Poop_Gold.png')}
-                itemname="Gold Poop"
-            />
-            <Emojibox
-                itemimg={require('./../images/Emoji/Source_Angel_Happy.png')}
-                itemname=" Happy Angel"
-            />
-        </div>
-        </div>
-            </div>
+  <div>
+    <div className="Selectan">
+      <h1>Emoji Charms</h1>
+    </div>
+    <div className="EmojiboxGroupScroll">
+      <div className="EmojiboxGroup">
+        {charms.map((charm, index) => (
+          <Emojibox
+            itemimg={require(`./../images/Emoji/${charm.image}.png`)}
+            itemname={charm.name}
+            key={index}
+          />
+        ))}
+      </div>
+    </div>
+  </div>
 )
 
 export default Charms
---
+++ src/constants/charms.js
@@ -0,0 +1,8 @@
+export const charms = [
+  { name: 'Call Me', image: 'CallMe' },
+  { name: 'Broken Heart', image: 'Heart_Broken' },
+  { name: 'Heart', image: 'Heart' },
+  { name: 'Kiss', image: 'Candy_Shop' },
+  { name: 'Gold Poop', image: 'Poop_Gold' },
+  { name: 'Happy Angel', image: 'Source_Angel_Happy' },
+]
