e0acdba694d4168eec83506a4903149bdf65a868
Angelo Cantone <angelocantone@imac-di-angelo.homenet.telecomitalia.it>
2019-03-07 12:18:32 +0100

    fix 2

--- src/pages/Hero.js
+++ src/pages/Hero.js
@@ -18,75 +18,15 @@ export default props => {
     <Layout>
       <div style={{ height: '100%' }}>
         <Page loader={'bar'} color={'#A9A9A9'} size={4} duration={1}>
-          <li key={hero.name}>
             <Heroes
               bgdesktop={require(`./../images/BG/` + path + `_Desktop.jpg`)}
               bgtablet={require(`./../images/BG/` + path + `_Desktop.jpg`)}
               bgmobile={require(`./../images/BG/` + path + `_Desktop.jpg`)}
+              heroname={hero.name}
               role={hero.type.join(', ')}
               description={hero.description}
               spotlight={hero.spotlight}
             />
-          </li>
-          <div className="Title">
-            <h1>3D MODEL</h1>
-            <div className="line" />
-          </div>
-          <Models heroname={hero.name} />
-          <div className="Title">
-            <h1>SKILLS (CLICK FOR MORE)</h1>
-            <div className="line" />
-          </div>
-          <div className="SkillboxGroupScroll">
-            <div className="SkillboxGroup">
-              {hero.skills.map((skill, index) => (
-                <Skillbox
-                  video={skill.video}
-                  title={skill.name}
-                  subtitle={skill.type}
-                  image={skill.image}
-                  text={skill.text}
-                  stats={skill.stats || []}
-                  key={index}
-                />
-              ))}
-            </div>
-          </div>
-          <div className="Title">
-            <h1>TALENTS (LVL 1 + % PER LVL)</h1>
-            <div className="line" />
-          </div>
-          <div className="TalentboxGroupScroll">
-            <div className="TalentboxGroup">
-              {hero.talents.map((talent, index) => (
-                <Talentbox
-                  title={talent.name}
-                  image={`${hero.name}_${talent.type}.png`}
-                  subtitle={talent.type.toUpperCase()}
-                  color={talent.color}
-                  text={talent.text}
-                  key={index}
-                />
-              ))}
-            </div>
-          </div>
-          <div className="Title">
-            {hero.skins.length ? <h1>SKINS</h1> : null}
-            <div className="line" />
-          </div>
-          <div className="SkinboxGroupScroll">
-            <div className="SkinboxGroup">
-              {hero.skins.map((skin, index) => (
-                <Skinbox
-                  color={skin.color}
-                  title={skin.name}
-                  text={skin.type}
-                  image={skin.image}
-                  key={index}
-                />
-              ))}
-            </div>
-          </div>
         </Page>
       </div>
     </Layout>
