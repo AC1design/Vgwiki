b6f7f7a609769f7e0253136e82eb5163c930aa23
Angelo Cantone <angelocantone@imac-di-angelo.homenet.telecomitalia.it>
2019-06-18 19:05:48 +0200

    fix 4.4 home

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,7 +29,7 @@ const IndexPage = props => (
           </div>
           <div className="HeroGroup">
             <h1>VAINGLORY WIKI</h1>
-            <p>UPDATE 4.3</p>
+            <p>UPDATE 4.4</p>
           </div>
         </div>
       </MediaQuery>
@@ -45,7 +45,7 @@ const IndexPage = props => (
           </div>
           <div className="HeroGroup">
             <h1>VAINGLORY WIKI</h1>
-            <p>UPDATE 4.3</p>
+            <p>UPDATE 4.4</p>
           </div>
         </div>
       </MediaQuery>
