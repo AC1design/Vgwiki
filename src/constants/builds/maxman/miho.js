e5537c6de8a803ad389cfe074440b1dea8225e3a
Skillz <gankstarsleague@gmail.com>
2019-07-18 18:21:26 -0400

    add miho build const file

---
+++ src/constants/builds/maxman/miho.js
@@ -0,0 +1,34 @@
+export const frontmatter = {
+  name: `MaxMan-4.5-Miho`,
+  path: `/builds/MaxMan-4.5-Miho`,
+  title: `MIHO COMPLITE GUIDE UPDATE 4.5`,
+  version: `4.5`,
+  creatorImage: `download`,
+  builds: [
+    {
+      name: `Starter`,
+      items: [
+        { name: `Aegis`, image: `aegis` },
+        { name: `Aftershock`, image: `aftershock` },
+        { name: `Alternating Current`, image: `alternating-current` },
+        { name: `Atlas Pauldron`, image: `atlas-pauldron` },
+        { name: `Barbed Needle`, image: `barbed-needle` },
+        { name: `Blazing Salvo`, image: `blazing-salvo` },
+      ],
+    },
+    {
+      name: `Core`,
+      items: [
+        { name: `Bonesaw`, image: `bonesaw` },
+        { name: `Book of Eulogies`, image: `book-of-eulogies` },
+      ],
+    },
+    {
+      name: `Situational`,
+      items: [
+        { name: `Breaking Point`, image: `breaking-point` },
+        { name: `Broken Myth`, image: `broken-myth` },
+      ],
+    },
+  ],
+}
